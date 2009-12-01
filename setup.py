9635d950d3f69a7b038890d4315bebcfc9d95e73
Joaquim Rocha <jrocha@igalia.com>
2009-12-01 17:57:02 +0100

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('src')
+from distutils.core import setup
+from SeriesFinale.lib import constants
+
+setup(name = constants.SF_NAME,
+     version = constants.SF_VERSION,
+     description = constants.SF_DESCRIPTION,
+     author = 'Joaquim Rocha',
+     author_email = 'jrocha@igalia.com',
+     url = constants.SF_URL,
+     license = 'GPL v3',
+     packages = ['SeriesFinale', 'SeriesFinale.lib',
+                 'jsonpickle'],
+     package_dir = {'': 'src'},
+     scripts = ['seriesfinale'],
+     data_files = [('/usr/share/icons/hicolor/scalable/apps', ['data/seriesfinale.png']
+                   ),
+                   ('/usr/share/applications/hildon', ['data/seriesfinale.desktop']
+                   ),
+                   ]
+     )
