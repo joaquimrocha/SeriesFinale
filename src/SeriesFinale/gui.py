6431bb97a16cc938040cbf790bcc587d5ebcfb26
Juan A. Suarez Romero <jasuarez@igalia.com>
2010-02-26 09:59:29 +0100

    Update copyright

--- src/SeriesFinale/gui.py
+++ src/SeriesFinale/gui.py
@@ -3,6 +3,7 @@
 ###########################################################################
 #    SeriesFinale
 #    Copyright (C) 2009 Joaquim Rocha <jrocha@igalia.com>
+#    Diablo version: Juan A. Suarez Romero <jasuarez@igalia.com>
 # 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
