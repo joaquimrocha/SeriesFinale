13084a691af061e975fa427bf30e816961390ac6
Juan A. Suarez Romero <jasuarez@igalia.com>
2010-04-19 12:24:31 +0200

    Add author copyright to thetvdbapi.py

--- src/SeriesFinale/lib/thetvdbapi.py
+++ src/SeriesFinale/lib/thetvdbapi.py
@@ -1,6 +1,7 @@
 """
 thetvdb.com Python API
 (c) 2009 James Smith (http://loopj.com)
+(c) 2010 Juan A. Suarez Romero <jasuarez@igalia.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
