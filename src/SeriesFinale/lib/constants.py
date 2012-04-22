ce1b8ad228d87128c9bd7a0ae1103e08fe64f79f
Juan A. Suarez Romero <jasuarez@igalia.com>
2012-04-22 16:40:10 +0200

    Revert "Update for release 0.6.9"
    This reverts commit d1fe11d82c9681a8f945b673898a91827fcc1f0e.
    
    We did not release 0.6.9 yet.

--- src/SeriesFinale/lib/constants.py
+++ src/SeriesFinale/lib/constants.py
@@ -23,7 +23,7 @@ import sys
 
 SF_NAME = 'SeriesFinale'
 SF_COMPACT_NAME = 'seriesfinale'
-SF_VERSION = '0.6.9'
+SF_VERSION = '0.6.8'
 SF_DESCRIPTION = 'SeriesFinale is a TV series browser and tracker application'
 SF_URL = 'http://www.igalia.com'
 SF_COPYRIGHT = 'Copyright © 2010-2011 Igalia S. L.'
