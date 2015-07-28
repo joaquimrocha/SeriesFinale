0dd25c741bb86681f3a518ef1dc794b9a362faae
Graham Inggs <graham@nerve.org.za>
2015-07-28 12:56:09 +0200

    Update for release 0.6.11

--- src/SeriesFinale/lib/constants.py
+++ src/SeriesFinale/lib/constants.py
@@ -24,7 +24,7 @@ import gtk
 
 SF_NAME = 'SeriesFinale'
 SF_COMPACT_NAME = 'seriesfinale'
-SF_VERSION = '0.6.10'
+SF_VERSION = '0.6.11'
 SF_DESCRIPTION = 'SeriesFinale is a TV series browser and tracker application'
 SF_URL = 'http://www.igalia.com'
 SF_COPYRIGHT = 'Copyright © 2011-2015 Joaquim Rocha\n' \
